adce271ed947d736503f2a45fed4254edbadd15d
George Damion lannister <gdamion-@at-b1.21-school.ru>
2019-04-08 11:44:13 +0300

    sss

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 12:03:52 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/04/07 17:31:58 by gdamion-         ###   ########.fr       */
+/*   Updated: 2019/04/08 11:36:37 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 # define PUSH_SWAP_H
 
 # include "common.h"
+
+void	magic(t_lswap *stack_one, t_lswap *stack_two, t_lswap *cmds);
+void	result(	t_lswap	*cmds);
 
 #endif
--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -3,17 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pcollio- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gdamion- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/25 12:03:41 by gdamion-          #+#    #+#             */
-/*   Updated: 2019/04/05 13:19:25 by pcollio-         ###   ########.fr       */
+/*   Updated: 2019/04/08 11:40:08 by gdamion-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
+{
+	t_lswap	*cmds;
+	t_lswap	*stack_one;
+	t_lswap *stack_two;
+
+	if (!(stack_one = (t_lswap*)malloc(sizeof(t_list)))
+		|| !(cmds = (t_lswap*)malloc(sizeof(t_list))))
+		error();
+	process_stack(&argc, &argv, stack_one);
+	magic(stack_one, stack_two, cmds);
+	result(cmds);
+	return (0);
+}
+
+void	magic(t_lswap *stack_one, t_lswap *stack_two, t_lswap *cmds)
 {
 
-	return (0);
+}
+
+void	result(	t_lswap	*cmds)
+{
+	while (cmds != NULL)
+	{
+		(cmds->num == 1) ? ft_putendl("sa") : 1;
+		(cmds->num == 2) ? ft_putendl("sb") : 1;
+		(cmds->num == 3) ? ft_putendl("ss") : 1;
+		(cmds->num == 4) ? ft_putendl("pa") : 1;
+		(cmds->num == 5) ? ft_putendl("pb") : 1;
+		(cmds->num == 6) ? ft_putendl("ra") : 1;
+		(cmds->num == 7) ? ft_putendl("rb") : 1;
+		(cmds->num == 8) ? ft_putendl("rr") : 1;
+		(cmds->num == 9) ? ft_putendl("rra") : 1;
+		(cmds->num == 10) ? ft_putendl("rrb") : 1;
+		(cmds->num == 11) ? ft_putendl("rrr") : 1;
+		cmds = cmds->next;
+	}
+	exit(0);
 }
